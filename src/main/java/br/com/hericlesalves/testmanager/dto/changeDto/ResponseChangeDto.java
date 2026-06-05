8c286c451e925fac6c28f25a5dcfb29a6b6d38d7
Hericles <hericlesreal@gmail.com>
2026-06-04 23:03:37 -0300

    adicionado o campo id em alguns dos dtos para que na uri na controller, eu consiga retornar corretamente a location no header

--- src/main/java/br/com/hericlesalves/testmanager/dto/changeDto/ResponseChangeDto.java
+++ src/main/java/br/com/hericlesalves/testmanager/dto/changeDto/ResponseChangeDto.java
@@ -4,6 +4,7 @@ import br.com.hericlesalves.testmanager.model.enums.ChangePriority;
 import br.com.hericlesalves.testmanager.model.enums.ChangeStatus;
 
 public record ResponseChangeDto(
+        Long id,
         String name,
         Integer numberChange,
         String description,
--- src/main/java/br/com/hericlesalves/testmanager/dto/testCaseDto/ResponseTestCaseDto.java
+++ src/main/java/br/com/hericlesalves/testmanager/dto/testCaseDto/ResponseTestCaseDto.java
@@ -3,6 +3,7 @@ package br.com.hericlesalves.testmanager.dto.testCaseDto;
 import jakarta.validation.constraints.NotBlank;
 
 public record ResponseTestCaseDto(
+        Long id,
         String title,
         String expectedResult,
         String steps
